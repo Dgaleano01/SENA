3d375179ed7b187f341963ec960ad64a574243e3
Dgaleano01 <degaleanocarmona@gmail.com>
2023-05-26 18:44:15 -0500

    Firebase

--- Richard/angular firebase/src/app/app.module.ts
+++ Richard/angular firebase/src/app/app.module.ts
@@ -8,6 +8,12 @@ import { MaterialModule } from './material.module';
 import {HttpClientModule} from '@angular/common/http';
 import { CrateEmployeesComponent } from './components/crate-employees/crate-employees.component';
 import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+
+
 
 @NgModule({
   declarations: [
@@ -22,6 +28,9 @@ import { ListEmployeesComponent } from './components/list-employees/list-employe
     MaterialModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase), 
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Richard/angular firebase/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAAbOj0hRKc5dSvISQPL1xtWh6_KCoilBs",
+        authDomain: "empleados-app-715d1.firebaseapp.com",
+        projectId: "empleados-app-715d1",
+        storageBucket: "empleados-app-715d1.appspot.com",
+        messagingSenderId: "954138351678",
+        appId: "1:954138351678:web:4da574f8d705825913996f"
+    }
+  };
